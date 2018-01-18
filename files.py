ccc67eaadad96f5ba9fdcc6ce28bc8f8081e1d5f
Imre Bartis <imre.bartis@siili.com>
2018-01-18 08:45:20 +0200

    write to a file

--- files.py
+++ files.py
@@ -5,25 +5,25 @@
 
 # f.close()
 
-with open('animals.csv', 'r') as f:
-    print(f.read())
+# with open('animals.csv', 'r') as f:
+#     print(f.read())
     
-import csv
-with open('animals.csv', 'r') as f:
-    animals = csv.reader(f)
-    for row in animals:
-        if row[-1] == 'True':
-            print("{0} is a {1} and is housebroken".format(row[0], row[1]))
-        elif row[-1] == 'False':
-            print("{0} is a {1} and is not housebroken!".format(row[0], row[1]))
+# import csv
+# with open('animals.csv', 'r') as f:
+#     animals = csv.reader(f)
+#     for row in animals:
+#         if row[-1] == 'True':
+#             print("{0} is a {1} and is housebroken".format(row[0], row[1]))
+#         elif row[-1] == 'False':
+#             print("{0} is a {1} and is not housebroken!".format(row[0], row[1]))
 
-import json
-with open('animals.json', 'r') as r:
-    data = json.load(r)
-    for row in data:
-        print(row['type'])
+# import json
+# with open('animals.json', 'r') as r:
+#     data = json.load(r)
+#     for row in data:
+#         print(row['type'])
 
-# f = open('cars.txt', 'a')
+# f = open('cars.txt', 'a') # 'a' is for appending
 # cars = ['chevy', 'tesla', 'ford']
 # for car in cars:
 #     f.write(car + '\n')
@@ -35,11 +35,11 @@ with open('animals.json', 'r') as r:
 #     for car in cars:
 #         f.write(car + '\n')
 
-# cars = [
-#     {"make": "chevy"},
-#     {"make": "tesla"},
-#     {"make": "porsche"}
-# ]
-# import json
-# with open('cars.json', 'w') as f:
-#     json.dump(cars, f)
+cars = [
+    {"make": "chevy"},
+    {"make": "tesla"},
+    {"make": "porsche"}
+]
+import json
+with open('cars.json', 'w') as f:
+    json.dump(cars, f)
